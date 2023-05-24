78e0799a567bcf371d50bbe847bc4ca48fc5dab2
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-05-24 17:02:12 -0300

    Adicionando o CORS

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ const express = require('express')
 const router = require('./router')
 const cors = require("cors")
 
-const app  = express()
+const app = express()
 
 app.use(cors())
+app.use(cors({
+    origin: '*'
+}))
 app.use(express.json())
 
 //Toda requisição vai cair dentro do Router
